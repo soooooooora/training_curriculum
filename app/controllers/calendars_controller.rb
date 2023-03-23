c254ec9b870eb337cf96f201c7d04e05e5b6afb1
soooooooora <127271130+soooooooora@users.noreply.github.com>
2023-03-23 17:00:54 +0900

    Issue2の修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -2,13 +2,13 @@ class CalendarsController < ApplicationController
 
   # １週間のカレンダーと予定が表示されるページ
   def index
-    getWeek
+    get_Week
     @plan = Plan.new
   end
 
   # 予定の保存
   def create
-    plan.create(plan_params)
+    Plan.create(plan_params)
     redirect_to action: :index
   end
 
